50d31d589bc2422189da48924c9c3736baea6968
27801248 <27801248@qq.com>
2026-09-02 09:18:38 +0800

    Update NovaCoreKitUI.m

--- NovaCoreKitUI.m
+++ NovaCoreKitUI.m
@@ -1,188 +1,120 @@
-//
-//  NovaCoreKitUI.m
-//  NovaCoreKit
-//  UI 组件实现：Toast、加载指示器、Alert 弹窗
-//
 #import "NovaCoreKitUI.h"
-
-static const NSTimeInterval kToastDefaultDuration = 2.0;
-static const CGFloat kToastCornerRadius = 10.0;
-static const CGFloat kToastVerticalMargin = 80.0;
-static const NSInteger kToastTag = 20260901;
-static const NSInteger kActivityTag = 20260902;
 
 #pragma mark - Toast
 
 @implementation NovaCoreKitToast
 
 + (void)showToast:(NSString *)message {
-    UIWindow *window = [self keyWindow];
-    if (!window) return;
-    [self showToast:message onView:window duration:kToastDefaultDuration position:NovaCoreKitToastPositionBottom];
+    [self showToast:message onView:nil duration:2.0 position:NovaCoreKitToastPositionBottom];
 }
 
-+ (void)showToast:(NSString *)message
-           onView:(UIView *)view
-         duration:(NSTimeInterval)duration
-         position:(NovaCoreKitToastPosition)position {
-    if (!view || message.length == 0) return;
-
-    // 移除旧 Toast
-    [self hideToast:view];
-
-    UILabel *label = [[UILabel alloc] init];
-    label.text = message;
-    label.textColor = [UIColor whiteColor];
-    label.font = [UIFont systemFontOfSize:14 weight:UIFontWeightMedium];
-    label.numberOfLines = 0;
-    label.textAlignment = NSTextAlignmentCenter;
-    label.backgroundColor = [UIColor colorWithWhite:0 alpha:0.8];
-    label.layer.cornerRadius = kToastCornerRadius;
-    label.layer.masksToBounds = YES;
-    label.tag = kToastTag;
-
-    CGSize maxSize = CGSizeMake(view.bounds.size.width - 80, CGFLOAT_MAX);
-    CGSize textSize = [message boundingRectWithSize:maxSize
++ (void)showToast:(NSString *)message onView:(UIView *)view duration:(NSTimeInterval)duration position:(NovaCoreKitToastPosition)position {
+    if (!message || message.length == 0) return;
+    
+    UIView *targetView = view ?: [self keyWindow];
+    if (!targetView) return;
+    
+    UILabel *toast = [[UILabel alloc] init];
+    toast.text = message;
+    toast.textColor = [UIColor whiteColor];
+    toast.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.75];
+    toast.font = [UIFont systemFontOfSize:14];
+    toast.textAlignment = NSTextAlignmentCenter;
+    toast.numberOfLines = 0;
+    toast.layer.cornerRadius = 8;
+    toast.clipsToBounds = YES;
+    toast.alpha = 0;
+    
+    CGSize textSize = [message boundingRectWithSize:CGSizeMake(targetView.bounds.size.width - 80, CGFLOAT_MAX)
                                             options:NSStringDrawingUsesLineFragmentOrigin
-                                         attributes:@{NSFontAttributeName: label.font}
+                                         attributes:@{NSFontAttributeName: toast.font}
                                             context:nil].size;
-    label.frame = CGRectMake(0, 0, textSize.width + 32, textSize.height + 16);
-
-    CGFloat centerX = view.bounds.size.width / 2.0;
-    CGFloat centerY;
+    
+    CGFloat toastWidth = textSize.width + 32;
+    CGFloat toastHeight = textSize.height + 16;
+    CGFloat x = (targetView.bounds.size.width - toastWidth) / 2;
+    CGFloat y;
+    
     switch (position) {
         case NovaCoreKitToastPositionTop:
-            centerY = kToastVerticalMargin + label.bounds.size.height / 2.0;
-            break;
-        case NovaCoreKitToastPositionBottom:
-            centerY = view.bounds.size.height - kToastVerticalMargin - label.bounds.size.height / 2.0;
+            y = 80;
             break;
         case NovaCoreKitToastPositionCenter:
+            y = (targetView.bounds.size.height - toastHeight) / 2;
+            break;
+        case NovaCoreKitToastPositionBottom:
         default:
-            centerY = view.bounds.size.height / 2.0;
+            y = targetView.bounds.size.height - toastHeight - 80;
             break;
     }
-    label.center = CGPointMake(centerX, centerY);
-    label.alpha = 0;
-    [view addSubview:label];
-
-    [UIView animateWithDuration:0.25 animations:^{
-        label.alpha = 1.0;
+    
+    toast.frame = CGRectMake(x, y, toastWidth, toastHeight);
+    [targetView addSubview:toast];
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        toast.alpha = 1;
     } completion:^(BOOL finished) {
-        [UIView animateWithDuration:0.25
-                              delay:duration
-                            options:UIViewAnimationOptionCurveEaseInOut
-                         animations:^{
-            label.alpha = 0;
+        [UIView animateWithDuration:0.3 delay:duration options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            toast.alpha = 0;
         } completion:^(BOOL finished) {
-            [label removeFromSuperview];
+            [toast removeFromSuperview];
         }];
     }];
 }
 
-+ (void)hideToast:(UIView *)view {
-    if (!view) return;
-    for (UIView *subview in view.subviews) {
-        if (subview.tag == kToastTag) {
-            [subview removeFromSuperview];
-        }
++ (UIView *)keyWindow {
+    UIApplication *app = [UIApplication sharedApplication];
+    if ([app.delegate respondsToSelector:@selector(window)]) {
+        return app.delegate.window;
     }
-}
-
-+ (UIWindow *)keyWindow {
-    if (@available(iOS 13.0, *)) {
-        for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
-            if (scene.activationState == UISceneActivationStateForegroundActive) {
-                for (UIWindow *window in scene.windows) {
-                    if (window.isKeyWindow) return window;
-                }
-            }
-        }
-    }
-    return [UIApplication sharedApplication].keyWindow;
+    return app.keyWindow;
 }
 
 @end
 
-#pragma mark - 加载指示器
+#pragma mark - Activity
 
 @implementation NovaCoreKitActivity
 
 + (void)showWithText:(NSString *)text {
-    UIWindow *window = [self keyWindow];
-    if (!window) return;
-    [self showOnView:window text:text];
-}
-
-+ (void)showOnView:(UIView *)view text:(NSString *)text {
-    if (!view) return;
-    [self hideOnView:view];
-
-    UIView *container = [[UIView alloc] init];
-    container.backgroundColor = [UIColor colorWithWhite:0 alpha:0.7];
-    container.layer.cornerRadius = 12.0;
-    container.tag = kActivityTag;
-    container.translatesAutoresizingMaskIntoConstraints = NO;
-
+    UIView *targetView = [NovaCoreKitToast keyWindow];
+    if (!targetView) return;
+    
+    UIView *bg = [[UIView alloc] initWithFrame:targetView.bounds];
+    bg.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.4];
+    bg.tag = 99991;
+    
+    UIView *box = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 120, 100)];
+    box.center = targetView.center;
+    box.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.8];
+    box.layer.cornerRadius = 10;
+    
     UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
-    indicator.translatesAutoresizingMaskIntoConstraints = NO;
-    [container addSubview:indicator];
+    indicator.center = CGPointMake(60, 40);
     [indicator startAnimating];
-
-    UILabel *label = [[UILabel alloc] init];
-    label.text = text;
+    [box addSubview:indicator];
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 65, 120, 25)];
+    label.text = text ?: @"加载中...";
     label.textColor = [UIColor whiteColor];
-    label.font = [UIFont systemFontOfSize:14];
+    label.font = [UIFont systemFontOfSize:13];
     label.textAlignment = NSTextAlignmentCenter;
-    label.translatesAutoresizingMaskIntoConstraints = NO;
-    [container addSubview:label];
-
-    [view addSubview:container];
-
-    // 约束
-    [NSLayoutConstraint activateConstraints:@[
-        [container.centerXAnchor constraintEqualToAnchor:view.centerXAnchor],
-        [container.centerYAnchor constraintEqualToAnchor:view.centerYAnchor],
-        [container.widthAnchor constraintGreaterThanOrEqualToConstant:120],
-        [container.heightAnchor constraintGreaterThanOrEqualToConstant:100],
-        [indicator.topAnchor constraintEqualToAnchor:container.topAnchor constant:20],
-        [indicator.centerXAnchor constraintEqualToAnchor:container.centerXAnchor],
-        [label.topAnchor constraintEqualToAnchor:indicator.bottomAnchor constant:12],
-        [label.leadingAnchor constraintEqualToAnchor:container.leadingAnchor constant:16],
-        [label.trailingAnchor constraintEqualToAnchor:container.trailingAnchor constant:-16],
-        [label.bottomAnchor constraintEqualToAnchor:container.bottomAnchor constant:-16],
-    ]];
+    [box addSubview:label];
+    
+    [bg addSubview:box];
+    [targetView addSubview:bg];
 }
 
 + (void)hide {
-    UIWindow *window = [self keyWindow];
-    if (window) [self hideOnView:window];
-}
-
-+ (void)hideOnView:(UIView *)view {
-    if (!view) return;
-    for (UIView *subview in view.subviews) {
-        if (subview.tag == kActivityTag) {
-            [UIView animateWithDuration:0.2 animations:^{
-                subview.alpha = 0;
-            } completion:^(BOOL finished) {
-                [subview removeFromSuperview];
-            }];
-        }
+    UIView *targetView = [NovaCoreKitToast keyWindow];
+    UIView *bg = [targetView viewWithTag:99991];
+    if (bg) {
+        [UIView animateWithDuration:0.2 animations:^{
+            bg.alpha = 0;
+        } completion:^(BOOL finished) {
+            [bg removeFromSuperview];
+        }];
     }
-}
-
-+ (UIWindow *)keyWindow {
-    if (@available(iOS 13.0, *)) {
-        for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
-            if (scene.activationState == UISceneActivationStateForegroundActive) {
-                for (UIWindow *window in scene.windows) {
-                    if (window.isKeyWindow) return window;
-                }
-            }
-        }
-    }
-    return [UIApplication sharedApplication].keyWindow;
 }
 
 @end
@@ -191,82 +123,33 @@ static const NSInteger kActivityTag = 20260902;
 
 @implementation NovaCoreKitAlert
 
-+ (void)showAlertWithTitle:(NSString *)title
-                   message:(NSString *)message
-                 okHandler:(void (^)(void))okHandler {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title
-                                                                   message:message
-                                                            preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"确定"
-                                                 style:UIAlertActionStyleDefault
-                                               handler:^(UIAlertAction * _Nonnull action) {
++ (void)showAlertWithTitle:(NSString *)title message:(NSString *)message okHandler:(void (^)(void))okHandler {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
         if (okHandler) okHandler();
-    }];
-    [alert addAction:ok];
-    [self presentAlert:alert];
+    }]];
+    
+    UIViewController *rootVC = [NovaCoreKitToast keyWindow].rootViewController;
+    while (rootVC.presentedViewController) {
+        rootVC = rootVC.presentedViewController;
+    }
+    [rootVC presentViewController:alert animated:YES completion:nil];
 }
 
-+ (void)showConfirmWithTitle:(NSString *)title
-                     message:(NSString *)message
-                   okHandler:(void (^)(void))okHandler
-               cancelHandler:(void (^)(void))cancelHandler {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title
-                                                                   message:message
-                                                            preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"取消"
-                                                     style:UIAlertActionStyleCancel
-                                                   handler:^(UIAlertAction * _Nonnull action) {
++ (void)showConfirmWithTitle:(NSString *)title message:(NSString *)message okHandler:(void (^)(void))okHandler cancelHandler:(void (^)(void))cancelHandler {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
         if (cancelHandler) cancelHandler();
-    }];
-    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"确定"
-                                                 style:UIAlertActionStyleDefault
-                                               handler:^(UIAlertAction * _Nonnull action) {
+    }]];
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
         if (okHandler) okHandler();
-    }];
-    [alert addAction:cancel];
-    [alert addAction:ok];
-    [self presentAlert:alert];
-}
-
-+ (void)showAlertWithTitle:(NSString *)title
-                   message:(NSString *)message
-                   actions:(NSArray<UIAlertAction *> *)actions {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title
-                                                                   message:message
-                                                            preferredStyle:UIAlertControllerStyleAlert];
-    for (UIAlertAction *action in actions) {
-        [alert addAction:action];
+    }]];
+    
+    UIViewController *rootVC = [NovaCoreKitToast keyWindow].rootViewController;
+    while (rootVC.presentedViewController) {
+        rootVC = rootVC.presentedViewController;
     }
-    [self presentAlert:alert];
-}
-
-+ (void)presentAlert:(UIAlertController *)alert {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        UIViewController *root = [self topViewController];
-        if (root) {
-            [root presentViewController:alert animated:YES completion:nil];
-        }
-    });
-}
-
-+ (UIViewController *)topViewController {
-    UIWindow *window = nil;
-    if (@available(iOS 13.0, *)) {
-        for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
-            if (scene.activationState == UISceneActivationStateForegroundActive) {
-                for (UIWindow *w in scene.windows) {
-                    if (w.isKeyWindow) { window = w; break; }
-                }
-            }
-        }
-    }
-    if (!window) window = [UIApplication sharedApplication].keyWindow;
-
-    UIViewController *vc = window.rootViewController;
-    while (vc.presentedViewController) {
-        vc = vc.presentedViewController;
-    }
-    return vc;
+    [rootVC presentViewController:alert animated:YES completion:nil];
 }
 
 @end
